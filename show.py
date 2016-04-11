1c08515207afabbd283cb2ed3b64eb34eb3f3f52
evilsocket <evilsocket@gmail.com>
2016-04-11 15:34:13 +0200

    Fixed GPL version in comment header.

--- show.py
+++ show.py
@@ -5,7 +5,7 @@
 # http://www.evilsocket.net
 #
 # This file may be licensed under the terms of of the
-# GNU General Public License Version 2 (the ``GPL'').
+# GNU General Public License Version 3 (the ``GPL'').
 #
 # Software distributed under the License is distributed
 # on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
